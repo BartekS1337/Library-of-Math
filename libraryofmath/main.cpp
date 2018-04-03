b43e45b627a2e799e426b59bbfbc7dd663f688ca
Bartłomiej <38051442+BartekS1337@users.noreply.github.com>
2018-04-03 22:30:48 +0200

    Update main.cpp

--- libraryofmath/main.cpp
+++ libraryofmath/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << fibonacci(6)<< endl; // 8
+    cout << power(3,3)<< endl; // 27
+    cout << NWD(16,24)<< endl; // 8
+    cout << factorial(7)<< endl; // 5040
+    cout << trianglemaker(1,-3,3,3,-3,5)<< endl; // 20 
+    
     return 0;
 }
