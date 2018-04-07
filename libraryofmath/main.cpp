12a84af0adfc59529acfe030165c65e121207ba6
Bartłomiej <38051442+BartekS1337@users.noreply.github.com>
2018-04-07 10:44:25 +0200

    Update main.cpp

--- libraryofmath/main.cpp
+++ libraryofmath/main.cpp
@@ -4,11 +4,45 @@ using namespace std;
 
 int main()
 {
-    cout << fibonacci(6)<< endl; // 8
-    cout << power(3,3)<< endl; // 27
-    cout << NWD(16,24)<< endl; // 8
-    cout << factorial(7)<< endl; // 5040
-    cout << trianglemaker(1,-3,3,3,-3,5)<< endl; // 20 
-    
+    int n, k , n2, a, b, mass, road, time, ax, ay, bx, by, cx, cy;
+
+
+    //fibonacci
+    cout<<"input number n"<<endl;
+    cin>>n;
+    cout<<"the "<<n<<"th number of Fibonacci sequence is:"<<fibonacci(n)<<endl;
+
+    //power
+    cout<<"input your base to power"<<endl;
+    cin>>k;
+    cout<<"input your index to power"<<endl;
+    cin>>n2;
+    cout<<"power of "<<k<<"^"<<n<<" ="<<power(k,n)<<endl;
+
+    //nwd
+    cout<<"input first number"<<endl;
+    cin>>a;
+    cout<<"input second number"<<endl;
+    cin>>b;
+    cout<<"The biggest common factor of "<<a<<" and "<<b<<" is "<< NWD(a,b)<<endl;
+
+    //kineticenergy
+    cout<<"To calculate the kinetic energy input mass"<<endl;
+    cin>>mass;
+    cout<<"input road"<<endl;
+    cin>>road;
+    cout<<"input time"<<endl;
+    cin>>time;
+    cout<<"Kinetic energy = "<<kineticenergy(mass,road,time)<<endl;
+
+    //Trianglemaker
+    cout<<"to calculate field of triangle input the coordinates(x,y) of node A"<<endl;
+    cin>>ax>>ay;
+    cout<<"input the coordinates(x,y) of node B"<<endl;
+    cin>>bx>>by;
+    cout<<"input the coordinates(x,y) of node C"<<endl;
+    cin>>cx>>cy;
+    cout<<"the field of your triangle is "<<trianglemaker(ax,ay,bx,by,cx,cy)<<endl;
+
     return 0;
 }
