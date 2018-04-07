6a92424d5d203f825bf5059db7bc6048614c79ab
Bartłomiej <38051442+BartekS1337@users.noreply.github.com>
2018-04-07 18:34:47 +0200

    Update mymath.h

--- libraryofmath/mymath.h
+++ libraryofmath/mymath.h
@@ -1,5 +1,5 @@
-int kineticenergy(double mass, double road, double time);
-int trianglemaker(int Ax, int Ay, int Bx, int By, int Cx, int Cy);
+double cylinderfield(double height, double ray);
+double trianglemaker(int Ax, int Ay, int Bx, int By, int Cx, int Cy);
 int power(int k, int n);
 int NWD(int, int);
 int fibonacci(int);
